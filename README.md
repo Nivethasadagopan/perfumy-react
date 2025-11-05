bd1759f3c363935826330e0f497ab7550fcfa68b
Nivethasadagopan <101561061+Nivethasadagopan@users.noreply.github.com>
2025-11-05 23:36:19 +0000

    Resolved conflicts

--- README.md
+++ README.md
@@ -1,4 +1,2 @@
-<<<<<<< HEAD
 # perfumy-react
-=======
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,22 @@
+
+
+//Navbar component
+
+function Navbar()
+{
+  return(
+     <nav>
+        <h1>Perfumy</h1>
+        <ul>
+          <li>Home |</li>
+          <li>Products |</li>
+          <li>About |</li>
+          <li>Contact Us</li>
+        </ul>
+      </nav>
+  )
+
+
+}
+
+export default Navbar
